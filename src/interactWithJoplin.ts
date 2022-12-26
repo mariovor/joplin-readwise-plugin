7c2bf3636641ad1f3dc869e44a4a3b973deb9cae
Mario Vormstein <gitlab@hquer.eu>
2022-12-26 15:23:29 +0100

    Do not create duplicates when creating notes

--- src/interactWithJoplin.ts
+++ src/interactWithJoplin.ts
@@ -27,10 +27,28 @@ export async function createOrGetPluginFolder(): Promise<string> {
         folderId = await createPluginFolder();
     } else if (foundFolder.length == 1) {
         let folderId = foundFolder[0].id;
-        console.log(`Found Plugin folder, tge id is ${folderId}`)
+        console.log(`Found Plugin folder, the id is ${folderId}`)
     } else throw Error('Error getting plugin folder')
 
     return folderId
+}
+
+async function getNote(title: string, parent_id: string): Promise<string> {
+    let noteId = null
+    const {_, items} = await joplin.data.get(['search'], {
+        query: title,
+        fields: 'id, parent_id'
+    })
+    if (items.length > 1) {
+        throw Error(`Found more than one entry for title "${title}"`)
+    } else if (items.length == 1) {
+        const item = items[0];
+        if (item.parentId != parent_id) {
+            throw Error(`Found one note with title ${title}, but it is in wrong folder.`)
+        }
+        noteId = item.id
+    }
+    return noteId
 }
 
 /**
@@ -41,8 +59,14 @@ export async function createNotes(highlights: HighlightExportResult[]) {
     const pluginFolderId = await createOrGetPluginFolder();
     for (let highlight of highlights) {
         const body = createNoteBody(highlight)
-        console.log(`Create note "${highlight.title}"`)
-        await joplin.data.post(['notes'], null, {body: body, title: highlight.title, parent_id: pluginFolderId});
+        const noteId = await getNote(highlight.title, pluginFolderId)
+        if (noteId) {
+            console.log(`Update note ${noteId}  with title "${highlight.title}"`)
+            await joplin.data.put(['notes', noteId], null, {body: body});
+        } else {
+            console.log(`Create note with title "${highlight.title}"`)
+            await joplin.data.post(['notes'], null, {body: body, title: highlight.title, parent_id: pluginFolderId});
+        }
     }
 }
 
